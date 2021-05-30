d77a6c59835d96ddb3294515797d8578c7e4a0ca
Pam94 <pamelafernandez94@gmail.com>
2021-05-30 19:51:19 +0200

    Font family configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'serif': ['Georgia', 'Times New Roman', 'serif'],
+      'sans': ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol']
+    },
     extend: {},
   },
   variants: {
